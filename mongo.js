b8582c2ed1ef73d5b6e0e361fcb2537e6c205119
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2026-03-25 20:03:20 +0000

    Remove MongoDB connection string from mongo.js
    Removed sensitive MongoDB connection string.

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 //const { mongoPath } = require('./config.json')
 
 const mongoPath =
-  'mongodb+srv://Ruairiw8:celtic62@virtual-casino-bot.o7cou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  ''
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
     useNewUrlParser: true,
